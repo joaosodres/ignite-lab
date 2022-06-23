f65d44ecfc961540647c9cbbf91e1b822824138c
joaosodres <joaov1070@gmail.com>
2022-06-23 12:18:40 -0300

    criação da estrutura visual do projeto

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4r2gtb70jrn01z6het992f3/master',
     cache: new InMemoryCache()
 });
